432a7261d229b2e885d94b780ce56880e04faf4c
Lite <zaidasouhil@gmail.com>
2022-02-13 13:03:56 -0800

    adding Full Talend, First SSIS

---
+++ Northwind/By Talend/Data_Warehouse/poms/jobs/process/dimcoustomer_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/jtds-1.3.1-patch-20190523.jar;../lib/talend_DB_mssqlUtil-1.4.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;dimcoustomer_0_1.jar;' dw.dimcoustomer_0_1.DIMCOUSTOMER  $args
